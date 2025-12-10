755993fd27950791a29a4f186654d25af168aa29
sendosend <johnsenberdin2930@gmail.com>
2025-12-10 21:53:07 +0800

    refactor the db and delete the initial data but the ticket errror inference still not fix

--- src/features/queries/get-ticket.ts
+++ src/features/queries/get-ticket.ts
@@ -1,10 +1,9 @@
-import { initialTickets } from "@/src/data";
-import { Ticket } from "../type";
+import prisma from "@/src/lib/prisma";
 
-export const getTicket = async (ticketId: string): Promise<Ticket | null> => {
-  const maybeTicket = initialTickets.find((ticket) => ticket.id === ticketId);
-
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
-  return new Promise((resolve) => resolve(maybeTicket || null));
+export const getTicket = async (id: string) => {
+  return await prisma.ticket.findUnique({
+    where: {
+      id,
+    },
+  });
 };
